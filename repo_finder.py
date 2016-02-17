010a8abdba8fb544399c1d069456114f6ea9bc87
jsbain <jsbain>
2016-02-17 11:04:57 +0000

    exclude trash folder

--- repo_finder.py
+++ repo_finder.py
@@ -1,10 +1,14 @@
 import dropdown, os, fnmatch
 class FilteredDirDropdown(dropdown.DropdownView):
-    def __init__(self,frame=(0,0,300,32),name='dropdown', filter='*',base=os.path.expanduser('~/Documents')):
+    def __init__(self,frame=(0,0,300,32),name='dropdown', filter='*',base=os.path.expanduser('~/Documents'), exclude='.Trash'):
         self.frame=frame
         self.filter=filter
         self.base=base
-        super(type(self),self).__init__(frame=frame,name=name,items=self.path_generator)
+        self.exclude=exclude
+        super(type(self),self).__init__(
+                   frame=frame, 
+                   name=name, 
+                   items=self.path_generator)
 
         
     def path_generator(self):
@@ -12,7 +16,5 @@ class FilteredDirDropdown(dropdown.DropdownView):
             for d in dirs:
                 if self.abort():
                     return
-                if fnmatch.fnmatch(d,self.filter):
+                if fnmatch.fnmatch(d,self.filter) and self.exclude not in rootpath:
                     yield os.path.relpath(rootpath,self.base)
-
-
